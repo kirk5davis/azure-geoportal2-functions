4746b1aa3aa6e467c5fe1d4aa3b02fffa35041ce
Davis <kirk.davis@watech.wa.gov>
2023-03-09 16:44:30 -0800

    init repo, fastapi testing

---
+++ GeoPortal2Webhook/__init__.py
@@ -0,0 +1,18 @@
+import logging
+import azure.functions as func
+from FastAPIApp import app
+
+@app.get("/sample")
+async def index():
+    return {
+    "info": "Try /hello/{name} for parameterized route.",
+    } 
+
+@app.get("/hello/{name}")
+async def get_name(name: str):
+    return {
+        "name": name,
+    }
+
+async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return await func.AsgiMiddleware(app).handle_async(req, context)
